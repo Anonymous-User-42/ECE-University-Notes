a3b137400243ff5d0080181c58bb81f55c3ce288
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-02-19 15:42:47 -0500

    Completed pascal.c

--- Semester 2/CPS 188 - Computer Programming Fundamentals/Laboratory/Lab Source Code/Lab 4/pascal.c
+++ Semester 2/CPS 188 - Computer Programming Fundamentals/Laboratory/Lab Source Code/Lab 4/pascal.c
@@ -1,1 +1,30 @@
 /*  Program to print the values of the Pascal's Triangle in Sequential Order    */
+
+#include <stdio.h>
+
+void pascal(void);
+
+void main(void)
+{
+    pascal();
+}
+
+void pascal(void)
+{
+    for (int i = 1; i <= 9; ++i)
+       {
+        
+        int value = 1;
+        
+        for (int j = 1; j <= 9; ++j){
+            
+            if (value != 0){
+            printf ("%d ", value);
+            }
+            
+            value = value * (i-j)/j;
+            
+          }
+            printf("\n");
+       }
+}
