e4596aa43f4d90817dce1fa2df74175edbbb3db7
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-03-19 22:56:20 -0400

    Initialized CPS 188 Lab 6 Problem 2

---
+++ Semester 2/CPS 188 - Computer Programming Fundamentals/Laboratory/Lab Source Code/Lab 6/Problem_2.c
@@ -0,0 +1,42 @@
+
+
+#include <stdio.h>
+#define ARRAY_SIZE 8
+//  finds the position of the smallest element in the subarray
+//  list[first] through list[last].
+//  Pre: first < last and elements 0 through last of array list are defined.
+//  Post: Returns the subscript k of the smallest element in the subarray;
+//  i.e., list[k] <= list[i] for all i in the subarray
+int get_min_range (int list[], int first, int last)
+{
+    /* complete the function here */
+}
+//  sorts the data in array list
+void select_sort(int list[], int n)
+{
+    int fill,   /*  index of first element in unsorted subarray */
+    temp,   /*  temporary storage   */
+    index_of_min;   /*  subscript of next smallest element  */
+    for (fill = 0; fill < n-1; ++fill)
+    {
+        /*  Find position of smallest element in unsorted subarray */
+        index_of_min = get_min_range (list, fill, n-1);
+        /* Exchange elements at fill and index_of_min */
+        if (fill != index_of_min)
+        {
+            temp = list[index_of_min];
+            list[index_of_min] = list[fill];
+            list[fill] = temp;
+        }
+    }
+}
+int main (void)
+{
+    int array[] = {67, 98, 23, 11, 47, 13, 94, 58};
+    int i;
+    select_sort (array, ARRAY_SIZE);
+    for (i=0; i < 8; ++i)
+    printf ("%d ", array[i]);
+    return (0);
+}
+
