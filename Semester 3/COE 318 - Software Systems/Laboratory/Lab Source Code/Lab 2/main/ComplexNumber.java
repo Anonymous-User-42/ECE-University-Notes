03055ffbe9d891f7094e09cc6dfcaba83378bc0b
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-09-26 00:42:20 -0400

    Deleted Comments from COE 318 Lab 2 main Source Code file

--- Semester 3/COE 318 - Software Systems/Laboratory/Lab Source Code/Lab 2/main/ComplexNumber.java
+++ Semester 3/COE 318 - Software Systems/Laboratory/Lab Source Code/Lab 2/main/ComplexNumber.java
@@ -7,138 +7,55 @@ package coe318.lab2;
  * @author Sayeed Ahamad
  */
 public class ComplexNumber {
-//Instance variable declarations
-
     double real, imaginary;
 
-    /**
-     * Construct a ComplexNumber given its real and imaginary parts.
-     *
-     * @param re The real component
-     * @param im The imaginary component
-     */
     public ComplexNumber(double re, double im) {
-//Initialize the instance variables
         real = re;
         imaginary = im;
     }
 
-    /**
-     * Returns the real component.
-     *
-     * @return the real
-     */
     public double getReal() {
         return real;	//A stub: to be fixed
     }
 
-    /**
-     * Returns the imaginary component.
-     *
-     * @return the imaginary
-     */
     public double getImaginary() {
         return imaginary;	//A stub: to be fixed
     }
 
-    /**
-     * Returns a new ComplexNumber number that is the negative of <em>this</em>.
-     * Note: the original ComplexNumber is <b>NOT</b>
-     * modified.
-     *
-     * @return -this
-     */
     public ComplexNumber negate() {
         return new ComplexNumber(-this.getReal(), -this.getImaginary()); //A stub: to be fixed
     }
 
-    /**
-     * Returns a new ComplexNumber that is the sum of <em>this</em>	and
-     * <em>z</em>. Note: the	original ComplexNumber is
-     * <b>NOT</b> modified.
-     *
-     * @param z
-     * @return this + z
-     */
     public ComplexNumber add(ComplexNumber z) {
         double r = this.getReal() + z.getReal();
         double i = this.getImaginary() + z.getImaginary();
         return new ComplexNumber(r, i);	//A stub: to be fixed
     }
 
-    /**
-     * Returns a new ComplexNumber that is the difference of <em>this</em>	and
-     * <em>z</em>. Note: the	original ComplexNumber is
-     * <b>NOT</b> modified.
-     *
-     * @param z
-     * @return this + z
-     */
     public ComplexNumber subtract(ComplexNumber z) {
         double r = this.getReal() - z.getReal();
         double i = this.getImaginary() - z.getImaginary();
         return new ComplexNumber(r, i);	//A stub: to be fixed
     }
 
-    /**
-     * Returns a new ComplexNumber that is the product of <em>this</em> and
-     * <em>z</em>. Note: the original ComplexNumber is
-     * <b>NOT</b> modified.
-     *
-     * @param z
-     * @return this * z
-     */
     public ComplexNumber multiply(ComplexNumber z) {
         double r = this.getReal() * z.getReal() - this.getImaginary() * z.getImaginary();
         double i = this.getReal() * z.getImaginary() + this.getImaginary() * z.getReal();
         return new ComplexNumber(r, i);	//A stub: to be fixed
     }
 
-    /**
-     * Returns a new ComplexNumber that is the reciprocal of <em>this</em>.
-     * Note: the original ComplexNumber is
-     * <b>NOT</b> modified.
-     *
-     * @return 1.0 / this
-     */
     public ComplexNumber reciprocal() {
         double r = this.getReal() / (Math.pow(getReal(), 2) + Math.pow(getImaginary(), 2));
         double i = -this.getImaginary() / (Math.pow(getReal(), 2) + Math.pow(getImaginary(), 2));
         return new ComplexNumber(r, i);	//A stub: to be fixed
     }
 
-    /**
-     * Returns a new ComplexNumber that is
-     * <em>this</em> divided by <em>z</em>. Note: the original ComplexNumber is
-     * <b>NOT</b> modified.
-     *
-     * @param z
-     * @return this / z
-     */
     public ComplexNumber divide(ComplexNumber z) {
         double r = (this.getReal() * z.getReal() + this.getImaginary() * z.getImaginary()) / (Math.pow(z.getReal(), 2) + Math.pow(z.getImaginary(), 2));
         double i = (this.getReal() * -z.getImaginary() + z.getReal() * this.getImaginary()) / (Math.pow(z.getReal(), 2) + Math.pow(z.getImaginary(), 2));
         return new ComplexNumber(r, i);	//A stub: to be fixed
     }
 
-    /**
-     * Returns a String representation of
-     * <em>this</em> in the format:
-     * <pre>
-     *	real +-(optional) i imaginary
-     * </pre> If the imaginary part is zero, only the real part is converted to
-     * a String. A "+" or "-" is placed between the real and imaginary parts
-     * depending on the the sign of the imagrinary part.
-     * <p>
-     * Examples:
-     * <pre>
-     *	..println(new ComplexNumber(0,0); -> "0.0"
-     *	..println(new ComplexNumber(1,1); -> "1.0 + i1.0"
-     *	..println(new ComplexNumber(1,-1); -> "1.0 - i1.0"
-     * </pre>
-     *
-     * @return the String representation.
-     */
     @Override
     public String toString() {
 //DO NOT MODIFY THIS CODE!
