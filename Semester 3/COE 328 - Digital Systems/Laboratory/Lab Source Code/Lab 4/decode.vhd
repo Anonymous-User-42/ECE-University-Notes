a7823a4fcfc5b1be48a4199a82ff10d669139cac
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2023-10-18 00:08:44 -0400

    Added COE 328 Lab 4 decode.vhd, encode.vhd

---
+++ Semester 3/COE 328 - Digital Systems/Laboratory/Lab Source Code/Lab 4/decode.vhd
@@ -0,0 +1,28 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY mux4to1 IS
+    PORT ( w0, w1, w2, w3 : IN STD_LOGIC;
+        s : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        f : OUT STD_LOGIC );
+END mux4to1 ;
+
+ARCHITECTURE Behavior OF mux4to1 IS
+BEGIN
+    WITH s SELECT
+    f <= w0 WHEN ”00”,
+        w1 WHEN ”01”,
+        w2 WHEN ”10”,
+        w3 WHEN OTHERS ;
+END Behavior;
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+PACKAGE mux4to1_package IS
+    COMPONENT mux4to1
+        PORT ( w0, w1, w2, w3 : IN STD_LOGIC ;
+            s : IN STD_LOGIC_VECTOR(1 DOWNTO 0) ;
+            f : OUT STD_LOGIC ) ;
+    END COMPONENT ;
+END mux4to1_package ;
