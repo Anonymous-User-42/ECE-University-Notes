f6db40b724dbb5831ab22672bdd8b9e74216286e
Anonymous-User-42 <mohammedsayeedahamad@gmail.com>
2024-07-04 00:49:59 -0400

    Completed COE 428 Lab 1

--- Semester 4.67/COE 428 - Engineering Algorithms and Data Structures/Laboratory/Lab Source Code/Lab 1/main/mySort.c
+++ Semester 4.67/COE 428 - Engineering Algorithms and Data Structures/Laboratory/Lab Source Code/Lab 1/main/mySort.c
@@ -1,3 +1,17 @@
-void mySort(int d[], unsigned int n)
+#include <stdio.h>
+
+void mySort(int array[], unsigned int num_elements)
 {
+    for (int i = 0; i < num_elements - 1; i++) {
+        for (int j = 0; j < num_elements - (i + 1); j++) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            } else {
+                continue;
+            }
+        }
+    }
+    
 }
--- Semester 4.67/COE 428 - Engineering Algorithms and Data Structures/Laboratory/Lab Source Code/Lab 1/main/sortMain.c
+++ Semester 4.67/COE 428 - Engineering Algorithms and Data Structures/Laboratory/Lab Source Code/Lab 1/main/sortMain.c
@@ -2,6 +2,46 @@
 #include <stdlib.h>
 #include "mySort.h"
 
-int main(int argc, char * argv[])
+int main()
+
 {
+    int arr_data[100000];   /*  Array of ints to sort */
+
+    int n_arr_data_items;   /*  Number of actual items in the array */
+
+    /* Test arr_data */
+
+    n_arr_data_items = 4;
+
+    arr_data[0] = 20;//10;
+
+    arr_data[1] = 40;//20;
+
+    arr_data[2] = 10;//30;
+
+    arr_data[3] = 30;//40;
+
+    mySort(arr_data, n_arr_data_items);
+
+    /*  Check that the arr_data array is sorted. */
+
+    for (int i = 0; i < n_arr_data_items - 1; i++)
+    {
+        if (arr_data[i] > arr_data[i + 1])
+        {
+            fprintf(stderr, "Sort error: arr_data[%d] (= %d)"
+                            " should be <= arr_data[%d] (= %d)- -aborting\n",
+                    i, arr_data[i], i + 1, arr_data[i + 1]);
+            exit(1);
+        }
+    }
+
+    /*  Print sorted array to stdout */
+
+    for (int i = 0; i < n_arr_data_items; i++)
+    {
+        printf("%d\n", arr_data[i]);
+    }
+
+    exit(0);
 }
